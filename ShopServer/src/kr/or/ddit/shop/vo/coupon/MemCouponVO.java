d60196d7384884fb8577b663a72e58bfb94165e9
hailey <haileyhe01@gmail.com>
2020-08-05 10:10:18 +0900

    VO 수정

--- ShopServer/src/kr/or/ddit/shop/vo/coupon/MemCouponVO.java
+++ ShopServer/src/kr/or/ddit/shop/vo/coupon/MemCouponVO.java
@@ -5,15 +5,17 @@ import java.io.Serializable;
 public class MemCouponVO implements Serializable{
 	private int coup_no;
 	private int memall_no;
+	private int coup_pcent;
 	
 	public MemCouponVO() {
 		
 	}
 	
-	public MemCouponVO(int coup_no, int mem_no) {
+	public MemCouponVO(int coup_no, int memall_no, int coup_pcent) {
 		super();
 		this.coup_no = coup_no;
-		this.memall_no = mem_no;
+		this.memall_no = memall_no;
+		this.coup_pcent = coup_pcent;
 	}
 	
 	public int getCoup_no() {
@@ -29,10 +31,20 @@ public class MemCouponVO implements Serializable{
 		this.memall_no = mem_no;
 	}
 
+	public int getCoup_pcent() {
+		return coup_pcent;
+	}
+	
+	public void setCoup_pcent(int coup_pcent) {
+		this.coup_pcent = coup_pcent;
+	}
+
 	@Override
 	public String toString() {
-		return "MemCouponVO [coup_no=" + coup_no + ", mem_no=" + memall_no + "]";
+		return "MemCouponVO [coup_no=" + coup_no + ", mem_no=" + memall_no + ", coup_pcent=" +
+							coup_pcent + "]";
 	}
+
 	
 	
 }
--- ShopServer/src/kr/or/ddit/shop/vo/order/OrderAll.java
+++ ShopServer/src/kr/or/ddit/shop/vo/order/OrderAll.java
@@ -5,50 +5,78 @@ import java.io.Serializable;
 public class OrderAll implements Serializable{
 	private int orderall_no;
 	private int order_no;
+	private int prod_no;
 	private int opt_no;
-	private int order_qty;
+	private int prod_ordqty;
+	private int prod_price;
 	
 	public OrderAll() {
 	
 	}
-	
-	public OrderAll(int orderall_no, int order_no, int opt_no, int order_qty) {
+
+	public OrderAll(int orderall_no, int order_no, int prod_no, int opt_no, int prod_ordqty, int prod_price) {
 		super();
 		this.orderall_no = orderall_no;
 		this.order_no = order_no;
+		this.prod_no = prod_no;
 		this.opt_no = opt_no;
-		this.order_qty = order_qty;
+		this.prod_ordqty = prod_ordqty;
+		this.prod_price = prod_price;
 	}
-	
+
 	public int getOrderall_no() {
 		return orderall_no;
 	}
+
 	public void setOrderall_no(int orderall_no) {
 		this.orderall_no = orderall_no;
 	}
+
 	public int getOrder_no() {
 		return order_no;
 	}
+
 	public void setOrder_no(int order_no) {
 		this.order_no = order_no;
 	}
+
+	public int getProd_no() {
+		return prod_no;
+	}
+
+	public void setProd_no(int prod_no) {
+		this.prod_no = prod_no;
+	}
+
 	public int getOpt_no() {
 		return opt_no;
 	}
+
 	public void setOpt_no(int opt_no) {
 		this.opt_no = opt_no;
 	}
-	public int getOrder_qty() {
-		return order_qty;
+
+	public int getProd_ordqty() {
+		return prod_ordqty;
 	}
-	public void setOrder_qty(int order_qty) {
-		this.order_qty = order_qty;
+
+	public void setProd_ordqty(int prod_ordqty) {
+		this.prod_ordqty = prod_ordqty;
+	}
+
+	public int getProd_price() {
+		return prod_price;
+	}
+
+	public void setProd_price(int prod_price) {
+		this.prod_price = prod_price;
 	}
 
 	@Override
 	public String toString() {
-		return "OrderAll [orderall_no=" + orderall_no + ", order_no=" + order_no + ", opt_no=" + opt_no + ", order_qty="
-				+ order_qty + "]";
+		return "OrderAll [orderall_no=" + orderall_no + ", order_no=" + order_no + ", prod_no=" + prod_no + ", opt_no="
+				+ opt_no + ", prod_ordqty=" + prod_ordqty + ", prod_price=" + prod_price + "]";
 	}
+
 	
 }
